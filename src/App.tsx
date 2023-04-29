60eba40cbefde819a0669778f04eed84711de77a
Vinicius Garcia <advviniciusgarcia@gmail.com>
2023-04-29 18:27:33 -0300

    develop: userDashboard in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { UserDashboard } from './pages/UserDashboard';
 import { UserProvider } from './providers/UserProvider';
 import { RoutesMain } from './routes';
 
 const App = () => {
   return (
-    // <div>
-    //   <UserProvider>
-    //     <RoutesMain />
-    //   </UserProvider>
-    // </div>
-    <UserDashboard/>
+    <div>
+      <UserProvider>
+        <RoutesMain />
+      </UserProvider>
+    </div>
   );
 
 };
