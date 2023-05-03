0e74740b3de15c6f20dd44d97a82183a66cf9429
Mislene Santana <contact.mislene@gmail.com>
2023-05-04 01:54:25 +0300

    feat: update lib toasts

--- src/pages/CommerceDashboard/index.tsx
+++ src/pages/CommerceDashboard/index.tsx
@@ -38,7 +38,7 @@ export const CommerceDashboard = () => {
     <StyledCommerceDashboard>
       <StyledHeaderContainer>
         <Header/>
-        {'tipar Header'}
+       <h1></h1>
         <button onClick={() => setIsEditProfileModalOpen(true)}>
           Edit profile
         </button>
