b9b77083d030dddeb723225ed93f8558ce4eab19
Vinicius Garcia <advviniciusgarcia@gmail.com>
2023-05-04 09:51:28 -0300

    protected routes

--- src/components/RoutesProtected/index.tsx
+++ src/components/RoutesProtected/index.tsx
@@ -2,18 +2,11 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 export const RoutesProtected = () => {
   const userToken = localStorage.getItem('@user:token');
-  const userId = localStorage.getItem('@user:id');
   const userCompanyToken = localStorage.getItem('@userCompany:token');
 
-  if (!userToken && !userId) {
-    <Navigate to='/' />;
+  if (!userCompanyToken && !userToken ) {
+    return <Navigate to='/' />;
   }
-  
-  if(!userCompanyToken && !userId) {
-    <Navigate to='/' />;
-  } 
 
-  return (
-      <Outlet />
-  );
+  return <Outlet />;
 };
